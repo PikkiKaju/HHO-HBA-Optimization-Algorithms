2682db1e0d5e56b586ca552ba7b17db4c0a1c1ab
PikkiKaju <igor.zmuda02@gmail.com>
2024-11-16 16:11:18 +0100

    Initial commit with WinForms

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Zastosowania_Sztucznej_Inteligencji
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
