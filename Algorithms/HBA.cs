303517a76474992f6a949733cd24b109d75476c4
PikkiKaju <igor.zmuda02@gmail.com>
2024-11-16 16:34:37 +0100

    Added the HHO and HBA Hoptimization algorithms

---
+++ Algorithms/HBA.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+class HoneyBadgerAlgorithm : IOptimizationAlgorithm
+{
+    public string Name { get; set; } = "HoneyBadgerAlgorithm";
+
+    public int PopulationSize { get; set; } = 30;
+    public int MaxIterations { get; set; } = 100;
+    public int Dimension { get; set; } = 30;
+
+    // HBO algorithm's beta hyperparameter
+    public double beta { get; set; } = 6;
+
+    // HBO algorithm's C hyperparameter
+    public double C { get; set; } = 2;
+
+    // Property for the best individual
+    public double[] XBest { get; set; }
+
+    // Property for the best individual's value of the fitness function 
+    public double FBest { get; set; }
+
+    // Property for keeping track of the number of fitness function calls
+    public int NumberOfEvaluationFitnessFunction { get; set; }
+
+    private static Random random = new Random();
+
+    // Individual Prey class
+    public class Prey
+    {
+        public double[] Position;
+        public double Fitness;
+
+        public Prey(int dimension)
+        {
+            Position = new double[dimension];
+        }
+    }
+
+    // Class constructor
+    public HoneyBadgerAlgorithm(int populationSize = 30, int maxIterations = 100, int dimension = 30, double beta = 6, double C = 2)
+    {
+        this.PopulationSize = populationSize;
+        this.MaxIterations = maxIterations;
+        this.Dimension = dimension;
+        this.beta = beta;
+        this.C = C;
+    }
+
+    // HoneyBadgerAlgorithm algorithm
+    public static OptimizationResult HBO(Func<double[], double> fitnessFunction, int populationSize = 30, int maxIterations = 100, int dimension = 30, double beta = 6, double C = 2)
+    {
+        List<Prey> population = new List<Prey>();
+        int numberOfEvaluationFitnessFunction = 0;
+
+        Prey bestPrey = new Prey(dimension);
+
+        // Population inicialization
+        for (int i = 0; i < populationSize; i++)
+        {
+            Prey prey = new Prey(dimension);
+            for (int d = 0; d < dimension; d++)
+                prey.Position[d] = random.NextDouble();
+            prey.Fitness = fitnessFunction(prey.Position);
+            population.Add(prey);
+
+            if (prey.Fitness < double.MaxValue)
+            {
+                bestPrey = prey;
+            }
+        }
+
+        for (int t = 0; t <= maxIterations; t++)
+        {
+            double alpha = HBOCalculation.UpdateDecreasingFactor(t, maxIterations);
+
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                // Calculate intensity vector for each dimension
+                double[] I = HBOCalculation.CalculateIntensity(population[i].Position, bestPrey.Position, beta);
+
+                // Create new position
+                double[] xnew = new double[dimension];
+                for (int d = 0; d < dimension; d++)
+                {
+                    if (random.NextDouble() < 0.5)
+                    {
+                        xnew[d] = population[i].Position[d] + alpha * I[d] + C * (2 * random.NextDouble() - 1);
+                    }
+                    else
+                    {
+                        xnew[d] = bestPrey.Position[d] + alpha * I[d] + C * (2 * random.NextDouble() - 1);
+                    }
+                }
+
+                // Evaluate the new position
+                double fnew = fitnessFunction(xnew);
+                numberOfEvaluationFitnessFunction++;
+
+                // Update the prey's position and fitness if better
+                if (fnew <= population[i].Fitness)
+                {
+                    population[i].Position = xnew;
+                    population[i].Fitness = fnew;
+                }
+
+                // Update the global best position if this is the best seen so far
+                if (fnew <= bestPrey.Fitness)
+                {
+                    bestPrey.Position = (double[])xnew.Clone();
+                    bestPrey.Fitness = fnew;
+                }
+            }
+        }
+
+        // Return the best result
+        return new OptimizationResult
+        {
+            xBest = bestPrey.Position,
+            fBest = bestPrey.Fitness,
+            numberOfEvaluationFitnessFunction = numberOfEvaluationFitnessFunction
+        };
+    }
+
+    public double Solve(Func<double[], double> fitnessFunction, int populationSize = 30, int maxIterations = 100, int dimension = 30)
+    {
+        OptimizationResult result = HBO(fitnessFunction, populationSize, maxIterations, dimension, beta, C);
+
+        // Assign the value of the optimization result to the globacl class properties
+        XBest = result.xBest;
+        FBest = result.fBest;
+        NumberOfEvaluationFitnessFunction = result.numberOfEvaluationFitnessFunction;
+
+        return FBest;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Najlepsza pozycja królika:\n");
+        foreach (var coord in XBest)
+        {
+            Console.WriteLine(coord);
+        }
+        Console.WriteLine($"\nWartość funkcji celu: {FBest}");
+        Console.WriteLine($"\nIlość wywołań funkcji dopasowania: {NumberOfEvaluationFitnessFunction}");
+    }
+}
+
+public class HBOCalculation
+{
+    public static double UpdateDecreasingFactor(int t, int maxIterations)
+    {
+        return 1.0 - (double)t / maxIterations;
+    }
+    public static double CalculateIntensity(double xi, double xprey, double beta)
+    {
+        return beta * Math.Abs(xi - xprey);
+    }
+
+    public static double[] CalculateIntensity(double[] xi, double[] xprey, double beta)
+    {
+        int dimension = xi.Length;
+        double[] intensity = new double[dimension];
+
+        for (int d = 0; d < dimension; d++)
+        {
+            intensity[d] = beta * Math.Abs(xi[d] - xprey[d]);
+        }
+
+        return intensity;
+    }
+}
---
+++ Algorithms/HHO.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+public class HarrisHawksOptimization : IOptimizationAlgorithm
+{
+    public string Name { get; set; }  = "HarrisHawksOptimization";
+
+    private int PopulationSize { get; set; } = 30;
+    private int MaxIterations { get; set; } = 100;
+    private int Dimension { get; set; } = 30;
+
+    // Property for the best individual
+    public double[] XBest { get; set; }
+
+    // Property for the best individual's value of the fitness function 
+    public double FBest { get; set; }
+
+    // Property for keeping track of the number of fitness function calls
+    public int NumberOfEvaluationFitnessFunction { get; set; }
+
+    private static Random random = new Random();
+
+    // Individual Hawk class
+    public class Hawk
+    {
+        public double[] Position;
+        public double Fitness;
+
+        public Hawk(int dimension)
+        {
+            Position = new double[dimension];
+        }
+    }
+
+    // Class constructor
+    public HarrisHawksOptimization(int populationSize = 30, int maxIterations = 100, int dimension = 30)
+    {
+        this.PopulationSize = populationSize;
+        this.MaxIterations = maxIterations;
+        this.Dimension = dimension;
+    }
+
+    // HarrisHawksOptimization algorithm
+    public static OptimizationResult HHO(Func<double[], double> fitnessFunction, int populationSize, int maxIterations, int dimension, double[] lowerBounds, double[] upperBounds)
+    {
+        List<Hawk> hawks = new List<Hawk>();
+        int numberOfEvaluationFitnessFunction = 0;
+
+        // Population inicialization
+        for (int i = 0; i < populationSize; i++)
+        {
+            Hawk hawk = new Hawk(dimension);
+            for (int d = 0; d < dimension; d++)
+                hawk.Position[d] = lowerBounds[d] + random.NextDouble() * (upperBounds[d] - lowerBounds[d]);
+            hawk.Fitness = fitnessFunction(hawk.Position);
+            hawks.Add(hawk);
+        }
+
+        Hawk bestHawk = hawks[0];
+
+        for (int t = 0; t < maxIterations; t++)
+        {
+            // Find the individual with the best position 
+            foreach (var hawk in hawks)
+            {
+                if (hawk.Fitness < bestHawk.Fitness)
+                    bestHawk = hawk;
+            }
+
+            foreach (var hawk in hawks)
+            {
+                double E0 = 2 * random.NextDouble() - 1;  // Initial hawk's energy
+                double J = 2 * (1 - random.NextDouble());  // Jump Rabbit's jump strength
+                double E = 2 * E0 * (1 - (double)t / maxIterations);  // Linear decrease in energy amount
+                double q = random.NextDouble(); // Random number to decide which formula is going to be used
+
+                // Exploration:
+                //  X(t +1) =
+                //    Xrand(t) − r1 | Xrand(t) − 2r2X(t) |        , q ≥ 0.5
+                //    (Xrabbit(t) − Xm(t)) − r3(LB + r4(UB − LB)) , q < 0.5
+
+                if (Math.Abs(E) >= 1)
+                {
+                    double[] Xrand = hawks[random.Next(hawks.Count)].Position;
+
+                    if (q >= 0.5)
+                    {
+                        for (int d = 0; d < dimension; d++)
+                        {
+                            double r1 = random.NextDouble();
+                            double r2 = random.NextDouble();
+                            hawk.Position[d] = Xrand[d] - r1 * Math.Abs(Xrand[d] - 2 * r2 * hawk.Position[d]);
+                        }
+                    }
+                    else
+                    {
+                        for (int d = 0; d < dimension; d++)
+                        {
+                            double r3 = random.NextDouble();
+                            double r4 = random.NextDouble();
+                            hawk.Position[d] = bestHawk.Position[d] - (bestHawk.Position[d] - hawk.Position[d]) - r3 * (lowerBounds[d] + r4 * (upperBounds[d] - lowerBounds[d]));
+                        }
+                    }
+                }
+
+                // Exploitation
+                else
+                {
+                    double r = random.NextDouble(); // Random number to decide which formula is going to be used
+                    double[] deltaX = new double[dimension]; // Location vector ∆X(t)
+                    for (int d = 0; d < dimension; d++)
+                    {
+                        // ∆X(t) = Xrabbit(t) − X(t)
+                        deltaX[d] = Math.Abs(bestHawk.Position[d] - hawk.Position[d]);
+                    }
+
+                    if (r >= 0.5 && Math.Abs(E) >= 0.5)
+                    {
+                        // Soft besiege
+                        // X(t +1) = ∆X(t)−E|JXrabbit(t) − X(t)|
+                        for (int d = 0; d < dimension; d++)
+                        {
+                            hawk.Position[d] = deltaX[d] - E * Math.Abs(J * bestHawk.Position[d] - hawk.Position[d]);
+                        }
+                    }
+                    else if (r >= 0.5 && Math.Abs(E) < 0.5)
+                    {
+                        // Hard besiege
+                        // X(t +1) = Xrabbit(t) − E |∆X(t)|
+                        for (int d = 0; d < dimension; d++)
+                        {
+                            hawk.Position[d] = bestHawk.Position[d] - E * Math.Abs(deltaX[d]);
+                        }
+                    }
+                    else if (r < 0.5 && Math.Abs(E) >= 0.5)
+                    {
+                        //  Y = Xrabbit(t) − E |JXrabbit(t) − X(t)|
+                        double[] Y = HHOCalculation.CalculateY(bestHawk.Position, hawk.Position, E, J, dimension);
+
+                        //  Z = Y + S × LF(D)
+                        double[] Z = HHOCalculation.CalculateZ(Y, dimension);
+
+                        // Soft besiege with progressive rapid dives
+                        // X(t + 1) =
+                        //    Y if F(Y) < F(X(t))
+                        //    Z if F(Z) < F(X(t))
+                        hawk.Position = fitnessFunction(Y) < fitnessFunction(hawk.Position) ? Y : Z;
+                    }
+                    else if (r < 0.5 && Math.Abs(E) < 0.5)
+                    {   
+                        //  Y = Xrabbit(t) − E |JXrabbit(t) − X(t)|
+                        double[] Y = HHOCalculation.CalculateY(bestHawk.Position, hawk.Position, E, J, dimension);
+
+                        //  Z = Y + S × LF(D)
+                        double[] Z = HHOCalculation.CalculateZ(Y, dimension);
+
+                        // Hard besiege with progressive rapid dives
+                        // X(t + 1) =
+                        //    Y if F(Y) < F(X(t))
+                        //    Z if F(Z) < F(X(t))
+                        hawk.Position = fitnessFunction(Y) < fitnessFunction(hawk.Position) ? Y : Z;
+                    }
+                }
+                numberOfEvaluationFitnessFunction++;
+                hawk.Fitness = fitnessFunction(hawk.Position); // Update the value of fitness function fot the best individual
+            }
+        }
+
+        // Return the best result
+        return new OptimizationResult
+        {
+            xBest = bestHawk.Position,
+            fBest = bestHawk.Fitness,
+            numberOfEvaluationFitnessFunction = numberOfEvaluationFitnessFunction
+        };   
+    }
+
+    public double Solve(Func<double[], double> fitnessFunction, int populationSize = 30, int maxIterations = 100, int dimension = 30)
+    {
+        double[] lowerBounds = new double[Dimension]; // Lower bounds
+        double[] upperBounds = new double[Dimension]; // Upper bounds
+
+        for (int i = 0; i < Dimension; i++)
+        {
+            lowerBounds[i] = -10;
+            upperBounds[i] = 10;
+        }
+
+        OptimizationResult result = HHO(fitnessFunction, populationSize, maxIterations, dimension, lowerBounds, upperBounds);
+
+        // Assign the value of the optimization result to the globacl class properties
+        XBest = result.xBest;
+        FBest = result.fBest;
+        NumberOfEvaluationFitnessFunction = result.numberOfEvaluationFitnessFunction;
+
+        return FBest;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Najlepsza pozycja królika:\n");
+        foreach (var coord in XBest)
+        {
+            Console.WriteLine(coord);
+        }
+        Console.WriteLine($"\nWartość funkcji celu: {FBest}");
+        Console.WriteLine($"\nIlość wywołań funkcji dopasowania: {NumberOfEvaluationFitnessFunction}");
+    }
+}
+
+public class HHOCalculation
+{
+    private static Random random = new Random();
+
+    public static double[] CalculateY(double[] bestHawkPosition, double[] hawkPosition, double E, double J, int dimension)
+    {
+        double[] Y = new double[dimension];
+
+        for (int d = 0; d < dimension; d++)
+        {
+            //  Y = Xrabbit(t) − E |JXrabbit(t) − X(t)|
+            Y[d] = bestHawkPosition[d] - E * Math.Abs(J * bestHawkPosition[d] - hawkPosition[d]);
+        }
+
+        return Y;
+    }
+
+    public static double[] CalculateZ(double[] Y, int D)
+    {
+        double beta = 1.5;
+        double[] S = GenerateRandomVector(D);
+        double[] levyFlight = LevyFlight(D, beta);
+        double[] Z = new double[D];
+
+        // Z = Y + S×LF(D)
+        for (int i = 0; i < D; i++)
+        {
+            Z[i] = Y[i] + S[i] * levyFlight[i];
+        }
+
+        return Z;
+    }
+
+    private static double[] GenerateRandomVector(int size)
+    {
+        //  wektor S z losowymi wartościami z zakresu (0, 1)
+        double[] S = new double[size];
+        for (int i = 0; i < size; i++)
+        {
+            S[i] = random.NextDouble();
+        }
+        return S;
+    }
+
+    private static double[] LevyFlight(int dimension, double beta)
+    {
+        double[] step = new double[dimension];
+        // Instalacja pakietu MathNet.Numerics 
+
+        //       ( Γ(1 + β) × sin( πβ / 2 )         )  ^  1
+        //  σ =  ( -------------------------------  )     -
+        //       ( Γ(1 + β / 2) × β × 2 (β - 1) / 2 )     β
+
+        double sigma = Math.Pow(
+            (MathNet.Numerics.SpecialFunctions.Gamma(1 + beta) * Math.Sin(Math.PI * beta / 2)) /
+            (MathNet.Numerics.SpecialFunctions.Gamma((1 + beta) / 2) * beta * Math.Pow(2, (beta - 1) / 2)),
+            1 / beta
+        );
+
+        for (int i = 0; i < dimension; i++)
+        {
+            // LF(x) = 0.01 × u × σ / |v| ^ (1 / β)
+            double u = random.NextDouble() * sigma;
+            double v = random.NextDouble();
+            step[i] = 0.01 * (u / Math.Pow(Math.Abs(v), 1 / beta));
+        }
+
+        return step;
+    }
+}
+
