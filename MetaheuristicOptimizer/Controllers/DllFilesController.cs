01551dd86f9256f69c9b2b5ecbc9ff85b41f524e
PikkiKaju <igor.zmuda02@gmail.com>
2025-02-05 15:59:30 +0100

    Added the file upload service and `/api/upload` endpoint

---
+++ MetaheuristicOptimizer/Controllers/DllFilesController.cs
@@ -0,0 +1,25 @@
+﻿using MetaheuristicOptimizer.Models;
+using MetaheuristicOptimizer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetaheuristicOptimizer.Controllers
+{
+    [ApiController]
+    [Route("api/upload")]
+    public class DllFilesController : ControllerBase
+    {
+        private readonly AlgorithmService _algorithmService = new();
+
+        [HttpPost("function")]
+        public IActionResult UploadFunctionFile(IFormFile file)
+        {
+            return Ok(new FileUploadService().UploadFunction(file));
+        }
+        
+        [HttpPost("algorithm")]
+        public IActionResult UploadAlgorithmFile(IFormFile file)
+        {
+            return Ok(new FileUploadService().UploadAlgorithm(file));
+        }
+    }
+}
---
+++ MetaheuristicOptimizer/Services/FileUploadService.cs
@@ -0,0 +1,52 @@
+﻿namespace MetaheuristicOptimizer.Services
+{
+    public class FileUploadService
+    {
+        public string UploadFunction(IFormFile file)
+        {
+            // extension
+            List<string> validExtensions = new List<string> { ".dll" };
+            string extension = Path.GetExtension(file.FileName);
+            if (!validExtensions.Contains(extension))
+            {
+                return $"Extension is not valid ({string.Join(".", validExtensions)})";
+            }
+            // file size
+            long size = file.Length;
+            if (size > (1 * 1024 * 1024))
+            {
+                return "Maximum size can be 1mb";
+            }
+            // name changing
+            string fileName = Guid.NewGuid().ToString() + extension;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", "Functions");
+            using FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
+            file.CopyTo(stream);
+
+            return fileName;
+        }
+        public string UploadAlgorithm(IFormFile file)
+        {
+            // extension
+            List<string> validExtensions = new List<string> { ".dll" };
+            string extension = Path.GetExtension(file.FileName);
+            if (!validExtensions.Contains(extension))
+            {
+                return $"Extension is not valid ({string.Join(".", validExtensions)})";
+            }
+            // file size
+            long size = file.Length;
+            if (size > (1 * 1024 * 1024))
+            {
+                return "Maximum size can be 1mb";
+            }
+            // name changing
+            string fileName = Guid.NewGuid().ToString() + extension;
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", "Algorithms");
+            using FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create);
+            file.CopyTo(stream);
+
+            return fileName;
+        }
+    }
+}
